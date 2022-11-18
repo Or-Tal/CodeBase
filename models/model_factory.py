6e9cacd48904829c22765eaf9e750073e6fa0040
Or-Tal <ortal1602@gmail.com>
2022-11-18 16:00:47 +0200

    added model_factory.py to support model object generation

---
+++ models/model_factory.py
@@ -0,0 +1,18 @@
+# This source code is licensed under MIT license found in the
+# LICENSE file in the root directory of this source tree.
+# Author: Or Tal.
+from dummy_ae import DummyAE
+
+
+class ModelFactory:
+
+    allowed_models = {
+        "dummy": DummyAE
+    }
+
+    @staticmethod
+    def get_model(model_args):
+        assert hasattr("model_class_name", model_args), "Please add 'model_class_name' to model configuration."
+        if model_args.model_class_name not in ModelFactory.allowed_models.keys():
+            raise ValueError("given model_class_name is not supported by model factory.")
+        return DummyAE(model_args)
