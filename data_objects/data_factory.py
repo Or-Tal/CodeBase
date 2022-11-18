ae6d2e50677540680e3ee9bd24e625b40a0d677c
Or-Tal <ortal1602@gmail.com>
2022-11-18 18:07:10 +0200

    added data factory

---
+++ data_objects/data_factory.py
@@ -0,0 +1,22 @@
+# This source code is licensed under MIT license found in the
+# LICENSE file in the root directory of this source tree.
+# Author: Or Tal.
+from typing import Tuple, Union
+from torch.utils.data.dataloader import DataLoader
+from data_objects.dummy_builder import DummyBuilder
+
+
+class DataFactory:
+
+    valid_builders = {
+        "dummy": DummyBuilder
+    }
+
+    @staticmethod
+    def get_loaders(data_config) -> Tuple[DataLoader, DataLoader, DataLoader, Union[DataLoader, None]]:
+        assert hasattr("data_builder_name", data_config), "Please add 'data_builder_name' to model configuration."
+        if data_config.data_builder_name not in DataFactory.valid_builders.keys():
+            raise ValueError(f"DsetBuilder: {data_config.data_builder_name} is not supported by DataFactory."
+                             f"\nPlease make sure implementation is valid.")
+        else:
+            return DataFactory.valid_builders[data_config.data_builder_name].get_tr_cv_tt_loaders(data_config)
